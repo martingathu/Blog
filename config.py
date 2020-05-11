f159635e0028732f01bb022270d720d13d9518ba
martingathu <martin5gathu@gmail.com>
2020-05-11 20:59:45 +0300

    add quotes API

---
+++ config.py
@@ -0,0 +1,49 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:auntyalice55@localhost/blog'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    UPLOADED_PHOTOS_DEST ='app/static/images'
+
+    # SECRET_KEY= os.environ.get("SECRET_KEY")
+    SECRET_KEY = '25616796'
+
+    ''' Api set up '''
+    QUOTE_API_BASE_URL ='GET http://quotes.stormconsultancy.co.uk/random.json'
+    
+    
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
