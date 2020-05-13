5b0989b60b57d5dbc350de912a874e1ff9b62ffa
martingathu <martin5gathu@gmail.com>
2020-05-13 08:01:26 +0300

    add a view function for quotes

--- app/views.py
+++ app/views.py
@@ -1,15 +1,14 @@
-from flask import render_template
-import app
-from .request import get_quote
+# from flask import render_template
+# import app
+# from .request import get_quote
 
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
 
-    # Getting a Random quote
-    random_quote = get_quote('random')
-    print(random_quote)
-    return render_template('index.html',random = random_quote)
+#     # Getting a Random quote
+#     quotes = get_quote()
+#     return render_template('index.html', quotes=quotes)
