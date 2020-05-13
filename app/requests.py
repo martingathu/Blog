5443300fe4c3abe78c24f943cb2c66149f455c96
martingathu <martin5gathu@gmail.com>
2020-05-13 07:54:56 +0300

    add a request for add blog posts

--- app/requests.py
+++ app/requests.py
@@ -1,49 +1,11 @@
-from app import app
 import urllib.request,json
-from app.models import Quote
+# base_url = None
 
-Quote = quote.Quote
-# Getting the movie base url
-base_url = app.config["QUOTE_API_BASE_URL"]
+def get_quotes():
+    get_quotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
 
-def get_quote(category):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_quote_url = base_url
-
-    with urllib.request.urlopen(get_quote_url) as url:
-        get_quote_data = url.read()
-        get_quote_response = json.loads(get_quote_data)
-
-        quote_results = None
-
-        if get_quote_response['results']:
-            quote_results_list = get_quote_response['results']
-            quote_results = process_results(quote_results_list)
-
-
-    return quote_results
-
-def process_results(movie_list):
-    '''
-    Function  that processes the quote result and transform them to a list of Objects
-
-    Args:
-        quote_list: A list of dictionaries that contain quote details
-
-    Returns :
-        quote_results: A list of quotes objects
-    '''
-    quote_results = []
-    for quote_item in quote_list:
-        id = quote_item.get('id')
-        author = quote_item.get('author')
-        quote = quote_item.get('quote')
-        permalink = quote_item.get('permalink')
+    with urllib.request.urlopen(get_quotes_url) as url:
+        quotes = url.read()
+        get_quotes_response = json.loads(quotes)
         
-        if quote:
-            quote_object = Quote(id,auther,permalink)
-            quote_results.append(quote_object)
-
-    return quote_results
+    return get_quotes_response
